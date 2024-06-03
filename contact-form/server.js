4ff21544016468b2bbf186a1512707969d45bcee
SandersJack <sandersjack141@gmail.com>
2024-06-03 15:40:32 +0100

    Added contact form

---
+++ contact-form/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const app = express();
+
+const config = require('./config');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(cors());
+
+app.post('/send_email', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: 'sandersjack141@gmail.com',
+            pass: config.emailPassword
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'sandersjack141@gmail.com',
+        subject: `New Contact Form Submission from ${name}`,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Email could not be sent.');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully!');
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
